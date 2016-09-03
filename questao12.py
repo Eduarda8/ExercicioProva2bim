6b4eb9012949ae97f5ef0835bcc9d8e805db13dc
Eduarda-Alves <eduarda.alves1170@gmail.com>
2016-09-03 16:23:18 -0300

    questões 11 á 15

---
+++ questao12.py
@@ -0,0 +1,6 @@
+#questão
+num = int (input("Informe o numero que deseja ser calculado na tabuada :"))
+print ("Diga o numero da tabuada: " , num ) 
+for i in range(1,11):  
+	print(str(num) + " x " + str(i) + " = " + str(num*i))
+
