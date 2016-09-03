7a2d9989e01cfee62cdebca77641974f00374352
Eduarda-Alves <eduarda.alves1170@gmail.com>
2016-09-03 18:06:21 -0300

    Continuação da atividade 16- 40

--- questao24.py
+++ questao24.py
@@ -1,22 +1,11 @@
 '''
-Questão 25 :
-Faça um programa que peça para n pessoas a sua idade, ao final o programa
-devera verificar se a média de idade da turma varia entre 0 e 25,26 e 60
-e maior que 60; e então, dizer se a turma é jovem, adulta ou idosa, conforme
-a média calculada.
+Questão 24 :
+Faça um programa que calcule o mostre a média aritmética de N notas.
 Resposta :
 '''
-Num = int(input("Digite um número : "))
-Soma = 0 
-cont = 0  
-for x in range(Num):
-	a = int(input("Digite uma idade na sequência : "))
-	Soma = Soma + a
-	cont = cont + 1
-media = Soma/cont
-if media>=0 and media<=25:
-  print("Jovem")
-elif media>=26 and media<=60:
-  print("Adulta")
-elif media>60:
-  print("Idosa")
+quantidade = int(input("Digite a quantidade de notas"))
+soma = 0
+
+for i in range(quantidade):
+    soma += int(input("Digite uma nota:"))
+print("A media e: ", soma/quantidade)
