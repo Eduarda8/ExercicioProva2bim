a630d394e165eebd8b69014d8a7c66c0e7111d61
Eduarda-Alves <eduarda.alves1170@gmail.com>
2016-09-03 17:45:12 -0300

    Continuação da atividade 16 - 40
    Questão refeita

--- questao24.py
+++ questao24.py
@@ -3,13 +3,9 @@ Questão 24 :
 Faça um programa que calcule o mostre a média aritmética de N notas.
 Resposta :
 '''
-Num = int(input("Digite um numero : "))
-Soma = 0 
-cont = 0  
-for x in range(Num):
-	a = int(input("Digite um numero na sequência : "))
-	if a > 0: 
-		Soma = Soma + a
-	cont = cont + 1
+quantidade = int(input("Digite a quantidade de notas"))
+soma = 0
 
-print ("O resultado é : ", Soma)
+for i in range(quantidade):
+    soma += int(input("Digite uma nota:"))
+print("A media e: ", soma/quantidade)
